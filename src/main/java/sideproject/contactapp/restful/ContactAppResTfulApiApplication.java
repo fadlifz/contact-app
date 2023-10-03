18a1b473ac48ab165fdf20dbf8b758a2dad00a6c
fadlifz <fadlifaturzaman@gmail.com>
2023-10-03 10:45:53 +0700

    first commit

---
+++ src/main/java/sideproject/contactapp/restful/ContactAppResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package sideproject.contactapp.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactAppResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactAppResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sideproject/contactapp/restful/ContactAppResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sideproject.contactapp.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactAppResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
